ace7e6494c6440d0a605ebe18587d942832c53a9
BH01\guilherme.meireles <guilherme.meireles@totvs.com.br>
2023-03-24 12:12:55 -0300

    acertando bug de uri

--- tradingtalk/RssReader.cs
+++ tradingtalk/RssReader.cs
@@ -45,7 +45,7 @@ internal class RssReader
           var messages = await rssInfo.GetNewMessagesAsync();
           if(messages?.Count>0)
           {
-            Console.WriteLine($"Novas mensagens ({messages.Count}) de {Uri}");
+            Console.WriteLine($"Novas mensagens ({messages.Count}) de {rssList.Uri}");
             var channel = _client.GetChannel(rssInfo.Channel) as IMessageChannel;
             foreach (string message in messages)
             {
