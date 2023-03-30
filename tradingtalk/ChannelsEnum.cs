4bd3d330ec64675ef07e3e7f71cc5409b668d91a
BH01\guilherme.meireles <guilherme.meireles@totvs.com.br>
2023-03-30 10:53:05 -0300

    alterando canais do discord de homol pra prod

--- tradingtalk/ChannelsEnum.cs
+++ tradingtalk/ChannelsEnum.cs
@@ -1,5 +1,9 @@
 internal enum ChannelsEnum : ulong
 {
+    HomologaçãoGeral = 1078455597751603250,
+    HomologaçãoAgro = 1078455487659528233,
+#if DEBUG
     HomologaçãoGeral = 1088279261426765985,
     HomologaçãoAgro = 1088279313054433320,
+#endif
 }
